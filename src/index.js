62110bb0103b8b57d1dba5fae5af5ae3876dfc5b
Juan Nunez <j.nunezc@uniandes.edu.co>
2023-09-26 09:18:12 -0500

    feat: casi terminado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from 'react-intl';
+import JobsList from "./components/jobsList";
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
-import JobsList from "./components/jobslist";
+let userLocale = navigator.language || navigator.userLanguage;
+if (userLocale.startsWith('es')) {
+    userLocale = 'es-ES';
+}
+else {
+    userLocale = 'en-US';
+}
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+ReactDOM.render(
+    <IntlProvider locale={userLocale} messages={userLocale === 'es-ES' ? localeEsMessages : localeEnMessages}>
+    <JobsList />
+    </IntlProvider>,
+    document.getElementById("root")
+);
